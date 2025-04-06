84d028da2f770b7fd391879879b5e37b39c7d9a4
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-04-07 03:25:57 +0530

    exploring get patch post

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello Namaste from the dashboard!");
+app.get("/user", (req, res) => {
+    res.send({ firstname: "John", lastname: "Doe" });
 });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello");
+app.post("/user", (req, res) => {
+    console.log("Save user data to database");
+    res.send({ message: "User data saved successfully!" });
+});
+
+app.delete("/user", (req, res) => {
+    res.send({ message: "User data deleted successfully!" });
 });
 
 app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
+    res.send("Hello from testing server!");
 });
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
