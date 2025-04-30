0e3d8491004451a00299b57a49fcf26ff7c5dbee
anujkamaljain <23UCS540@lnmiit.ac.in>
2025-05-01 05:27:37 +0530

    fix: Update error handling in user authentication middleware for missing token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if(!token){
-        throw new Error("Invalid token. Please login again.");
+      return res.status(401).send("Please Login!");
     }
     const decodedMessage = await jwt.verify(token, process.env.JWT_TOKEN_KEY);
     const { _id } = decodedMessage;
